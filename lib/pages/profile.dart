f78b3f4eb8f7db086c3dfe6a850336015bfdbb54
Naman Shergill <namanpreet@gmail.com>
2019-05-24 01:56:51 +0530

    Update profile.dart

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -7,115 +7,197 @@ final DFFont= 'Josefin';
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final _media = MediaQuery.of(context).size;
     return ListView(
+      padding: EdgeInsets.all(0),
       physics: BouncingScrollPhysics(),
-      padding: EdgeInsets.only(
-        left: 20,
-        right: 20,
-        top: 70,
-      ),
       children: <Widget>[
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Profile',
-              style: TextStyle(
-                fontSize: 35,
-                fontWeight: FontWeight.bold,
-                fontFamily: DFFont,
-              ),
-            ),
-            FlatButton(
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-                onPressed: (){
-                  },
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.mode_edit,
-                      color: Colors.grey,
-                      size: 20,
+        Container(
+          constraints: BoxConstraints(minHeight: 350),
+          color: Colors.grey.shade50,
+          height: _media.height / 2 - 120,
+          child: Stack(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Expanded(
+                    flex: 5,
+                    child: Stack(
+                      children: <Widget>[
+                        Material(
+                          elevation: 4,
+                          child: Container(
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage('assets/images/bg.jpg'),
+                                    fit: BoxFit.cover
+                                )
+                            ),
+                          ),
+                        ),
+                        Opacity(
+                          opacity: 0.3,
+                          child: Container(
+                            color: Colors.black87,
+                          ),
+                        )
+                      ],
                     ),
-                    Text(
-                      'Edit',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey,
-                          fontFamily: DFFont,
-                          fontSize: 20
-                      ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(),
+                  )
+                ],
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  height: 160,
+                  width: _media.width - 30,
+                  child:
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        height: 140,
+                        width: 140,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                        ),
+                        child: Icon(Icons.account_circle, size: 140, color: Colors.black87,),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                top: _media.longestSide <= 775
+                    ? screenAwareSize(20, context)
+                    : screenAwareSize(35, context),
+                left: 10,
+                right: 10,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 70,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                'Profile',
+                                style: TextStyle(
+                                    fontSize: 50,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: "Josefin"),
+                              ),
+                              FlatButton(
+                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                                onPressed: (){
+                                },
+                                child: Row(
+                                  children: <Widget>[
+                                    Icon(
+                                      Icons.mode_edit,
+                                      color: Colors.grey,
+                                      size: 25,
+                                    ),
+                                    Text(
+                                      'Edit',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.grey,
+                                          fontFamily: DFFont,
+                                          fontSize: 25
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-            )
-          ],
-        ),
-        SizedBox(
-          height: 30,
-        ),
-        Text(
-          'Overview',
-          style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              inherit: true,
-              fontFamily: DFFont,
-              letterSpacing: 0.4
-          ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              height: 120,
-              width: 120,
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.white,
               ),
-              child: Icon(Icons.account_circle, size: 120, color: Colors.orangeAccent,),
-            )
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            colorCard('Username', 'Naman Shergill', context, Colors.green)
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            colorCard('E-mail address', 'namanpreet@gmail.com', context, Colors.blue)
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            colorCard('Phone Number', '7733088115', context, Colors.purpleAccent)
-          ],
-        ),
-        SizedBox(
-          height: 25,
-        ),
-        Text(
-          'Wallet',
-          style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              inherit: true,
-              fontFamily: DFFont,
-              letterSpacing: 0.4
+            ],
           ),
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            colorCard('Balance', 2000.toString(), context, Color(0xFFff3f5e))
-          ],
-        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 20, left: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                'Overview',
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    inherit: true,
+                    fontFamily: DFFont,
+                    letterSpacing: 0.4
+                ),
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  colorCard('Username', 'Naman Shergill', context, Colors.green)
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  colorCard('E-mail address', 'namanpreet@gmail.com', context, Colors.blue)
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  colorCard('Phone Number', '7733088115', context, Colors.purpleAccent)
+                ],
+              ),
+              SizedBox(
+                height: 45,
+              ),
+              Text(
+                'Wallet',
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    inherit: true,
+                    fontFamily: DFFont,
+                    letterSpacing: 0.4
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  colorCard('Balance', 2000.toString(), context, Color(0xFFff3f5e))
+                ],
+              ),
+              SizedBox(
+                height: 45,
+              ),
+            ],
+          ),
+        )
       ],
     );
   }
@@ -139,7 +221,8 @@ Widget colorCard(
               blurRadius: 16,
               spreadRadius: 0.2,
               offset: Offset(0, 8)),
-        ]),
+        ]
+    ),
     child: Material(
       borderRadius: BorderRadius.circular(15),
       color: color,

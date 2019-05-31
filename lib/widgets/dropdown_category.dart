b2813e27bb580fdf09e06bac70a16200c7ffc886
Naman Shergill <namanpreet@gmail.com>
2019-06-01 00:25:21 +0530

    Replaced dropwdown animated container with expansiontile

--- lib/widgets/dropdown_category.dart
+++ lib/widgets/dropdown_category.dart
@@ -15,83 +15,39 @@ class _categoryState extends State<category> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(top: 5),
+    final theme = Theme.of(context).copyWith(dividerColor: Colors.transparent);
+    return Theme(
+      data: theme,
       child: Material(
-        color: Colors.transparent,
-        child: Padding(
-          padding: EdgeInsets.only(
-              left: 20.0, right: 20,top: 5),
-          child: Column(
+        child: InkWell(
+          onTap: (){
+          },
+          child: ExpansionTile(
+            title: Text(
+               widget.title,
+               style: TextStyle(
+                 fontSize: 20,
+                 fontWeight: FontWeight.bold,
+                 fontFamily: 'Josefin',
+              ),
+            ),
             children: <Widget>[
-              Material(
-                color: Colors.transparent,
-                child: InkWell(
-                  onTap: (){
-                    setState(() {
-                      height = height == 0
-                          ? height = 60
-                          : height = 0
-                      ;
-                      icon = icon == Icons.arrow_drop_down
-                          ? icon =  Icons.arrow_drop_up
-                          : icon = Icons.arrow_drop_down
-                      ;
-                    });
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 5, right: 5),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          widget.title,
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Josefin',
-                          ),
-                        ),
-                        SizedBox(
-                          width: 5,
-                        ),
-                        AnimatedContainer(
-                          height: 40,
-                          duration: Duration(milliseconds: 200),
-                          child: Icon(
-                            icon,
-                            color: Colors.grey,
-                            size: 20,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 5,
-              ),
               Padding(
-                padding: const EdgeInsets.only(left: 5, right: 5),
-                child: AnimatedContainer(
-                  height: height,
-                  duration: Duration(milliseconds: 200),
-                  child: Text(
-                    widget.desc,
-                    textAlign: TextAlign.justify,
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 18,
-                      fontFamily: 'Josefin',
-                    ),
+                padding: EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                child: Text(
+                  widget.desc,
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 18,
+                    fontFamily: 'Josefin',
                   ),
                 ),
-              )
+              ),
             ],
           ),
         ),
-      ),
+      )
     );
   }
 }
